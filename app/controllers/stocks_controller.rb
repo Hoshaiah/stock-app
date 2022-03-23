2adae9592f0d16150b88ce23914b5e50433e6bc7
Hoshaiah <domingohoshaiah@gmail.com>
2022-03-23 17:59:06 +0800

    added index view to stocks and some preliminary code for create route (that have not yet been tested)

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,2 +1,26 @@
 class StocksController < ApplicationController
+    def index
+        @stocks = current_user.stocks.all
+    end
+
+    def create
+        @stock = current_user.stocks.build(symbol: transaction_params[:stock], average_price: transaction_params[:price], volume: transaction_params[:volume])
+
+        if @stock.save
+            redirect_to stocks_url, notice: "Stock was added to your portfolio"
+        end
+    end
+
+
+    def update
+        @stock = current_user.stocks.where symbol:transaction_params[:stock]
+        
+        if params[:method] == "Buy"
+            @stock.volume += transaction_params[:volume]
+            @stock.average_price = (@stock.average_price * @stock.volume + transaction_params[:price] * transaction_params[:volume]) / @stock.volume + transaction_params[:volume]
+        elsif params[:method] == "Sell"
+            @stock.volume -= transaction_params[:volume]
+        end
+
+    end
 end
