51064de2e478e5a3326464ce8b36b74696f9e430
Hoshaiah <domingohoshaiah@gmail.com>
2022-03-25 03:11:12 +0800

    added links for admin navigations

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :admins
-  get "admin", to: "admins#admin_home", as: "admin_home"
-  get "admin/transactions", to: "admins#transactions", as: "transactions_all"
-  get "admin/users", to: "users#index", as: "users_all"
-  post "admin/users", to: "users#create", as: "create_user"
-  delete "admin/users/:id", to: "users#destroy", as: "destroy_user"
+  get "admin", to:"admins#admin_home", as: "admin_home"
+  get "admin/transactions", to:"admins#transactions", as: "transactions_all"
+  get "admin/users", to:"users#index", as: "users_all"
+  post "admin/users", to:"users#create", as: "create_user"
+  delete "admin/users/:id", to:"users#destroy", as: "destroy_user"
   get "admin/users/:id/edit", to:"users#edit", as: "edit_user"
   patch "admin/users/:id/edit", to:"users#update", as: "update_user"
-  get "admin/users/new", to: "users#new", as: "new_users"
+  get "admin/users/new", to:"users#new", as: "new_user"
 
   devise_for :users
   root to: "transactions#new", as: "home"
