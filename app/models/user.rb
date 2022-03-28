2db24f56a6f99ac85f6e29ea0bdaf3eec11e276f
Hoshaiah <domingohoshaiah@gmail.com>
2022-03-28 18:52:51 +0800

    last edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :transactions
-  has_many :stocks
+  has_many :transactions, :dependent => :destroy
+  has_many :stocks, :dependent => :destroy
 
   def buy_stock!(volume, stock)
     symbol = stock
